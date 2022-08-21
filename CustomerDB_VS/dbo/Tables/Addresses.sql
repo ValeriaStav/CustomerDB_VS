7968a203bd00b9229b5774bea6f34a2f239ec835
ValeriaStav <ValeriiaStavriadi@gmail.com>
2022-08-21 23:55:46 +0300

    Add project files.

---
+++ CustomerDB_VS/dbo/Tables/Addresses.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Addresses] (
+    [AddressId]    INT            IDENTITY (1, 1) NOT NULL,
+    [CustomerId]   INT            NOT NULL,
+    [AddressLine]  NVARCHAR (100) NOT NULL,
+    [AddressLine2] NVARCHAR (100) NULL,
+    [AddressType]  NVARCHAR (8)   NOT NULL,
+    [City]         NVARCHAR (50)  NOT NULL,
+    [PostalCode]   NVARCHAR (6)   NOT NULL,
+    [State]        NVARCHAR (20)  NOT NULL,
+    [Country]      NVARCHAR (30)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([AddressId] ASC),
+    CHECK ([AddressType]='Billing' OR [AddressType]='Shipping'),
+    CHECK ([Country]='Canada' OR [Country]='USA'),
+    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId])
+);
+
---
+++ CustomerDB_VS/dbo/Tables/Notes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [NoteId]     INT            IDENTITY (1, 1) NOT NULL,
+    [CustomerId] INT            NOT NULL,
+    [Note]       NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([NoteId] ASC),
+    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
