edd4b753aaff1a7ce95f980159ff05bcefb731b6
ValeriaStav <100225737+ValeriaStav@users.noreply.github.com>
2022-08-25 11:35:39 +0300

    Update and rename Notes.sql to Note.sql

--- CustomerDB_VS/dbo/Tables/Note.sql
+++ CustomerDB_VS/dbo/Tables/Note.sql
@@ -1,8 +1,8 @@
-﻿CREATE TABLE [dbo].[Notes] (
+﻿CREATE TABLE [dbo].[Note] (
     [NoteId]     INT            IDENTITY (1, 1) NOT NULL,
     [CustomerId] INT            NOT NULL,
     [Note]       NVARCHAR (255) NOT NULL,
     PRIMARY KEY CLUSTERED ([NoteId] ASC),
-    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId]) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([CustomerId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
